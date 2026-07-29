571534e2a6391b2360323510ddadffadd2e87506
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-07-29 09:47:34 +0200

    in this leason i lent how to collect user input from the tarminal and we have two wasy of getting user input useing scanf and fgets but scanf is limited and can noly get cantotinated caracters /init/float/double as it cant thens to stop before a white space and anything after is not stored while for fgets it gets what ever input the users enters with enter itselfs lol using the syntax fgets(varableName, varableSizeInMemory, stdin) now stdin is an input pipe that gets data from your keybord thats why enter is also a caracter from your input i also lent to clear the \n white space enter adds we do that with \0 just as \n is not visible to the user an its only read by the computer \0 tells the computer to clear the withspace in that point c rocks yo !

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(){
+    /*
     //first looking at c syntex it isent that far of from javascript 
     char name[] = "victory"; /* char a varible delcation keyword to store strings value
     char only stors a single carecter taking 1 byte in memory but to store a string with multiple bytes
@@ -9,6 +11,7 @@ int main(){
     tried storing a hole number in a caracter delacration but char []array taks up much byte as the caracters grow am think if each byte in char is one if i have
     say five caracters thats 5 bites
     */
+   /*
     int age= 22; // init a varable delaration key word use to store hole numbers int takes up 4 bytes in memory also init cant hold decimal values only hole so 45.23 is stored as 45 as it cant store decimal
     float rate= 12.5; // floart is used to store floating point numbers thats decimal numbers but floart is limited to how many remender values thats values after . we could store float stakes up 4 bytes in memory
     double doubleRate= 12.5998888893; // double just like float stores decimal values or floating point values but un like float it can store upto 15 values after the . decimal double takes up 8 bytes in memory as its float multibled by two  4 bytes * 2;
@@ -29,5 +32,33 @@ int main(){
     //note to run a program in c we first compile it how awsome is that i fucking compile my code my self i and see the assembly code in a.out file does see gets more intresting
     //also we need to retun value in our funtion call 0 for code runs smothly 1 for errors
     //time to compile :)
+    */
+
+    char product[20];
+    int qountity;
+    char validation[5];
+    int total=0;
+    printf("hello welcome to shopping demo what would you like to order \n");
+    fgets(product, 20, stdin);
+    product[strlen(product) - 1] = '\0';
+    printf("how many %-1s do you want to order: ", product);
+    scanf("%d", &qountity);
+    printf("is your older correct if its correct validate with \"yes\" if not validate with \"no\" your're getting %d \n", qountity);
+    printf("%s\n", product);
+    getchar();
+    fgets(validation, sizeof(validation), stdin);
+    validation[strlen(product) - 1]= '\0';
+    total= qountity * 2;
+        printf("great your total is %d thank you for shopping have a nice day\n", total);
+    //printf("%s", validation[0]);
+    //printf(validation);
+    /*
+    if(validation == "yes"){
+        int total= qountity * 2;
+        printf("great you total is %d thank you for shopping have a nice day\n", total);
+    }else{
+        printf("sorry we got your order wrong could you place it over again \n");
+    };
+    */
     return 0;
 }
