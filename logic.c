f69a5967daefe7c8eca2912eca47735637872dd7
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-07-30 12:55:12 +0200

    created push array method to push single caracter not multiple to the and off and array and still maintaing its refrance in memory in the stack

--- logic.c
+++ logic.c
@@ -2,35 +2,50 @@
 #include <stdbool.h>
 #include <strings.h>
 #include <stdlib.h>
-
-bool includes(char* pArray, char value);
-bool includesMultiple(char* pArray, char* pArrayValue);
-
-int  main(){
-    //am going to be creating some javaScript built in methods
+//am going to be creating some javaScript built in methods
     //1 .inclueds which is a linear seacrh that goes through the list and cheack if a value exist it returns true if it does if not it return false
     //2 .push and .pop // .push which takes a value and append it to the end of an array and //.pop removes a single value from the end of an erray
     //3 
-
+//
+//bool includes(char* pArray, char value);
+//bool includesMultiple(char* pArray, char* pArrayValue);
+char pushSingle(char *pArray, char value);
+int  main(){
+    
     //char name[]= "victory";
     //char find = 's';
+    //char name[]= "victory";
+    //char find[] = "victor";
     char name[]= "victory";
-    char find[] = "victor";
+    /*
+    char* name= malloc(20);
+    (name[0])= 'v';
+    (name[1])= 'i';
+    (name[2])= 'c';
+    (name[3])= 't';
+    (name[4])= 'o';
+    (name[5])= 'r';
+    (name[6])= 'y';
+    */
+   //scanf("%c", &name[0]);
+   //printf("name value %s \n", name);
+    char value = 'N';
     //char* pName = malloc(50);
     //printf("%ld \n", sizeof(name));
     //first am going to go with linear seach as .includes do then i check if i coud do binary search
     //int* pointer= &name;
     //printf("%p \n",  &name);
     //printf("those %c includes in %s : %s \n", find, name, (includes(name, find)) ? "true": "false");
-    printf("those %s includes in %s : %s \n", find, name, (includesMultiple(name, find)) ? "true": "false");
+    //printf("those %s includes in %s : %s \n", find, name, (includesMultiple(name, find)) ? "true": "false");
     //includes(name, find);
     //printf("name: %c\n", name[0]);
     //printf("pointer %p\n", pName);
     //free(pName);
+    pushSingle(name, value);
+    printf("finished modified array %s \n",name);
     return 1;
 }
-
-//1 .includes
+/*//1 .includes
 bool includes(char* pArray, char value){
     int length= sizeof(pArray) / sizeof(pArray[0]);
     //
@@ -59,3 +74,19 @@ bool includesMultiple(char* pArray, char* pArrayValue){
     if((validatedCarecters == lenthOfParentArray) && (validatedCarecters  == lenthOfChildArray)) return true;
     return false;
 };
+*/
+
+// 2 .push 
+char pushSingle(char *pArray, char value){
+    int lenthOfArray= sizeof(pArray) / sizeof((pArray[0]));
+    printf("original pointer %p \n", pArray);
+    printf("original array %s \n",(pArray));
+    for(int i=0; i< lenthOfArray; i++){
+        (pArray[i]) = (pArray[i]);
+        if((i + 1) == lenthOfArray){
+            (pArray[i]) = value;
+        }
+    }
+    printf("modified array %s \n",(pArray));
+    printf("modified pointer %p \n", pArray);
+}
