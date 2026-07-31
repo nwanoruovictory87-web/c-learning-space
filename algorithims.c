98f8172d27853de27450dd0ee9adb289cabeccd1
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-07-31 18:38:09 +0200

    modified search algorithim made search pointer to 2 first pointer starting at 0 and stoping at middle second pointer starting at middle and stoping at end so it scans half way from 0 - then can half way from middle repeat till it scan the hole number also with some values above 000000000000  x 4 stored in varables i max out my call stack memory funny it dosent true errors it just freazes up like in an infinite loop this search is a few seconds faster then linear search for un other list also got a bug when returing a value it return s below and nagative the value i returned don't know if my stack memory was masked at that point and can't return thr full value maybe i would store the varables in heap and refreance with pointer i tried but am not that good with pointers so i would study more and try to see how to improve the search algorithim

--- algorithims.c
+++ algorithims.c
@@ -3,18 +3,16 @@
 #include <time.h>
 #include <stdbool.h>
 
- int findValueAtIndex(long int superLongDataLength, long int find);
-
-
+int findValueAtIndex(long int superLongDataLength, long int find);
 int main(){
     //binary search
     //int length = 
-    long int superLongDataLength= 899889758944;
+    long int superLongDataLength= 8998897589;
     printf("before \n");
     //printf("size of count %p", (i));
     //int find =7589464;
    long int find = 7989487942;
-    for( long int i=0; i< superLongDataLength; i++){
+    for(long int i=0; i< superLongDataLength; i++){
       if(i == find){
         printf("linear search found data %ld \n", i);
         break;
@@ -31,42 +29,47 @@ int main(){
 
  int findValueAtIndex(long int superLongDataLength, long int find){
       long int forward= 0;
-      long int backward=0; 
+      long int middle=0; 
+      long incrementMiddle = superLongDataLength / 2;
       const long int maxItiration= 1000;
+      printf("size of %ld \n", sizeof(superLongDataLength));
       //printf("max value %ld \n", maxItiration);
       bool isValueFound= false;
-      while (((forward  + backward) < superLongDataLength) && !isValueFound){
+      while (((forward  + middle) < superLongDataLength) && !isValueFound){
       int countFoward=0;
     //
-    for(long int i =forward; i< (forward + maxItiration); i++){
-       //printf("forwards %ld \n", i);  
-       if((i + 1) >= (superLongDataLength - backward)) break;
+   long int forwardsLimit= (forward + maxItiration) > (superLongDataLength / 2) ? (superLongDataLength / 2) : (forward + maxItiration);
+   //printf("fowards limit %ld \n", forwardsLimit);
+    for(long int i =forward; i< forwardsLimit; i++){
+      // printf("forwards %ld \n", i);  
       if(i == find){
-            printf("found value at %ld index value is %ld moving fowards  moved %ld fowards  & %ld backwards\n", i, find, forward, backward);
+            printf("found value at %ld index value is %ld moving fowards  moved %ld fowards \n", i, find, (forward + countFoward));
             isValueFound = true;
-            return i;
+            return 0;
           }
         countFoward +=1;
+        forward +=1;
     }
-   forward += countFoward;
+   //forward += countFoward;
     //
-    int countBackword=0;
-    long int bacwardScaned= superLongDataLength - backward;
-    for(long int i =bacwardScaned; i > (bacwardScaned - maxItiration);  i--){
-        //printf("backwords %ld \n", i);
-        if((i + 1) <= forward) break;
+    int countMiddle=0;
+    for(long int i =incrementMiddle; i < superLongDataLength;  i++){
+        //printf("middle %ld \n", i);
         if(i == find){
-            printf("found value at %ld index value is %ld moving backwards moved %ld fowards  & %ld backwards\n", i, find, forward, backward);
+            printf("found value at %ld index value is %ld moving from middle  moved %ld started at %ld \n", i, find, (incrementMiddle +  countMiddle), (superLongDataLength / 2));
           isValueFound = true;
+          printf("index %ld \n", i);
             return i;
           }
-        countBackword +=1;
+        countMiddle +=1;
+        middle+=1;
     }
-   backward += countBackword;
+   incrementMiddle += countMiddle;
+   //middle += countMiddle;
    //printf("forwards %ld - middle %ld - backwards %ld \n", forward, middle, backward);
     }
-    printf("total value %ld\n", (forward + backward ));
-    if((forward  + backward ) == superLongDataLength){
+    printf("total value %ld\n", (forward + middle ));
+    if((forward  + middle ) == superLongDataLength){
       printf("No data found\n");
       return -1;
     }
